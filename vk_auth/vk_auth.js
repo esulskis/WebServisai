f887897598016cf2db73d650f6cbfd4737c1570c
Elvinas <e.sulskis2@gmail.com>
2017-02-24 13:59:12 +0200

    Pridėtas autorizacijos servisas

---
+++ vk_auth/vk_auth.js
@@ -0,0 +1,39 @@
+
+var http = require("http");
+var soap = require("soap");
+var xml = require('fs').readFileSync('vk_auth/vk_auth.wsdl', 'utf8');
+var userList = [
+  {"email":"ts1@email.com", "password":"psswrd"},
+{"e-mail":"ts2@email.com","password":"psswrd"}];
+
+
+var service = {
+   authenticationService: {
+        authenticationPort : {
+            authenticationOperation: function (args) {
+              var match = false;
+              for(user in userList){
+                if (user["email"] == args["email"] && user["password"] == args["password"]){
+                  match = true;
+                }
+
+              }
+                return {
+                    userValid: match
+                }
+            }
+        }
+    }
+};
+
+
+
+
+var server = http.createServer(function(request, response) {
+
+  response.end("404: Not Found: " + request.url);
+
+});
+
+server.listen(81);
+soap.listen(server, '/vk-auth', service, xml);
