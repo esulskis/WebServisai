75820e9a898c62a1e033ce10b64963a4fd454178
Elvinas <e.sulskis2@gmail.com>
2017-02-25 16:07:42 +0200

    sukurta sesija

--- app.js
+++ app.js
@@ -4,24 +4,26 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session')
+var debug = require('debug')('Express4');
 var routes = require('./app_server/routes/index');
-var users = require('./app_server/routes/users');
-require('./app_server/models/db');
-var app = express();
+//require('./app_server/models/db');
 
+var app = express();
 
 app.set('views', path.join(__dirname, "app_server",'views'));
 app.set('view engine', 'jade');
-
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({secret:"sdddsafa4sdf5",resave:false, saveUninitialized:true}))
+
+
 app.use('/', routes);
-app.use('/users', users);
+
 
 
 app.use(function(req, res, next) {
@@ -51,4 +53,8 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+app.set('port', process.env.PORT || 80);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
