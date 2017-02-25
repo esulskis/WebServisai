2761b93824deed846af730695a8919d2e3260b5d
Elvinas <e.sulskis2@gmail.com>
2017-02-25 15:59:37 +0200

    Pataisytas autorizacijos servisas

--- vk_auth/vk_auth.js
+++ vk_auth/vk_auth.js
@@ -3,17 +3,15 @@ var http = require("http");
 var soap = require("soap");
 var xml = require('fs').readFileSync('vk_auth/vk_auth.wsdl', 'utf8');
 var userList = [
-  {"email":"ts1@email.com", "password":"psswrd"},
-{"e-mail":"ts2@email.com","password":"psswrd"}];
-
-
+  {"email":"ts1@email.com", "password":"123"},
+{"e-mail":"ts2@email.com","password":"123"}];
 var service = {
    authenticationService: {
         authenticationPort : {
             authenticationOperation: function (args) {
               var match = false;
               for(user in userList){
-                if (user["email"] == args["email"] && user["password"] == args["password"]){
+                if (userList[user]["email"] == args["email"] && userList[user]["password"] == args["password"]){
                   match = true;
                 }
 
