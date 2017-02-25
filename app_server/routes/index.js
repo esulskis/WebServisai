25ffc1a95f35dd9c23e6471ec0e656cabba4673a
Elvinas <e.sulskis2@gmail.com>
2017-02-25 16:10:50 +0200

    Pridėtas login post metodas patikrinantis vartotojo prisijungima

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
+var soap = require('soap');
+var url = 'http://localhost:81/vk-auth?wsdl';
 
-
-/* GET home page. */
 
 router.get('/', ctrlMain.index);
 router.get('/login', ctrlMain.login);
+router.get('/logout',ctrlMain.logout);
+
+
+router.post("/login", function(request, response){
+  var args = {email:request.body["email"],
+      password:request.body["password"]}
+      console.log(args["email"]);
+soap.createClient(url, function(error, client) {
+
+if(error){
+  console.console.log(error);
+}
+client.authenticationOperation(args, function(err, result) {
+
+    if(result["userValid"]){
+      request.session.user = args["email"];
+      response.redirect('/');
+    }else {
+
+console.log(result["userValid"])
+
+    }
+
+      });
+});
+
+
+});
+
 
 
 module.exports = router;
